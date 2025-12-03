8aaa8c4737814858548ac0f4ff3cafcb886f1a0a
khaledxyz <khaledhalloua@outlook.com>
2025-12-03 09:40:29 +0100

    setup api

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,9 +1,53 @@
+import {
+  BadRequestException,
+  RequestMethod,
+  ValidationPipe,
+} from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
 import { NestFactory } from "@nestjs/core";
+
+import helmet from "helmet";
+import { Logger, LoggerErrorInterceptor } from "nestjs-pino";
 
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
+
+  const configService = app.get(ConfigService);
+
+  app.use(helmet({ noSniff: false }));
+
+  const allowedOrigins =
+    configService
+      .get<string>("ALLOWED_ORIGINS")
+      ?.split(",")
+      ?.map((origin) => origin.trim())
+      ?.filter((origin) => origin.length > 0) || [];
+
+  app.enableCors({
+    origin: allowedOrigins,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    credentials: true,
+  });
+
+  app.setGlobalPrefix(configService.get<string>("APP_PREFIX"), {
+    exclude: [{ path: "/", method: RequestMethod.GET }],
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      exceptionFactory:
+        configService.getOrThrow("NODE_ENV") === "production"
+          ? (_errors) => new BadRequestException("Bad Request")
+          : undefined, // undefined means use default behavior
+    })
+  );
+
+  app.useLogger(app.get(Logger));
+  app.useGlobalInterceptors(new LoggerErrorInterceptor());
+
+  await app.listen(configService.getOrThrow("APP_PORT"));
 }
 bootstrap();
