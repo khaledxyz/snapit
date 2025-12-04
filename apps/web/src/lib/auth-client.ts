371b132b0e040cd43c7cbb635a85bf290758ead1
khaledxyz <khaledhalloua@outlook.com>
2025-12-04 16:19:38 +0100

    feat: init better auth for the client

---
+++ apps/web/src/lib/auth-client.ts
@@ -0,0 +1,7 @@
+import { createAuthClient } from "better-auth/react";
+
+export const authClient = createAuthClient({
+	baseURL: import.meta.env.VITE_API_URL,
+	basePath: "/api/auth",
+	trustedOrigins: [import.meta.env.VITE_CLIENT_URL],
+});
