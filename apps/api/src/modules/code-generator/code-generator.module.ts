a3bea42d42a5b87b1d2c8cc983e69edbbfda8f5c
khaledxyz <khaledhalloua@outlook.com>
2025-12-07 11:52:51 +0100

    feat: create code generator module

---
+++ apps/api/src/modules/code-generator/code-generator.module.ts
@@ -0,0 +1,9 @@
+import { Module } from "@nestjs/common";
+
+import { CodeGeneratorService } from "./code-generator.service";
+
+@Module({
+  providers: [CodeGeneratorService],
+  exports: [CodeGeneratorService],
+})
+export class CodeGeneratorModule {}
---
+++ apps/api/src/modules/code-generator/code-generator.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from "@nestjs/common";
+
+import { customAlphabet } from "nanoid";
+
+@Injectable()
+export class CodeGeneratorService {
+  private readonly defaultAlphabet =
+    "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+  generate(length = 7, alphabet: string = this.defaultAlphabet): string {
+    const generator = customAlphabet(alphabet, length);
+    return generator();
+  }
+}
