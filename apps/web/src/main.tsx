71f2b4beb63875e85cd7f40b5ca8a4f77bf48acd
khaledxyz <khaledhalloua@outlook.com>
2025-12-09 14:20:41 +0100

    feat: setup @snapit/sdk client

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -3,12 +3,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import { setBaseURL } from "@snapit/sdk";
-
 import { App } from "./app";
 import "./index.css";
 
-setBaseURL(import.meta.env.VITE_API_URL || "http://localhost:5000");
+import { client } from "@snapit/sdk";
+
+client.setConfig({
+  baseUrl: import.meta.env.VITE_CLIENT_URL,
+  credentials: "include",
+});
 
 createRoot(document.getElementById("app")!).render(
   <StrictMode>
