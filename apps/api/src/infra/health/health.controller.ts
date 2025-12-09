65b1bc886246e3158fbb38ca18c58db4ff0d7df3
khaledxyz <khaledhalloua@outlook.com>
2025-12-09 10:10:14 +0100

    ignore health controller

--- apps/api/src/infra/health/health.controller.ts
+++ apps/api/src/infra/health/health.controller.ts
@@ -1,7 +1,9 @@
 import { Controller, Get } from "@nestjs/common";
+import { ApiExcludeController } from "@nestjs/swagger";
 import { HealthCheck, HealthCheckService } from "@nestjs/terminus";
 
 @Controller("health")
+@ApiExcludeController()
 export class HealthController {
   constructor(private readonly health: HealthCheckService) {}
 
