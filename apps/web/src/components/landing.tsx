e39f1c149dc5e9a0e94c18924ea62f0ea0945675
khaledxyz <khaledhalloua@outlook.com>
2025-12-25 12:13:25 +0100

    feat: create dither background

--- apps/web/src/components/landing.tsx
+++ apps/web/src/components/landing.tsx
@@ -1,25 +1,31 @@
+import Background from "./background";
 import { Shortener } from "./shortener";
 import { UrlsList } from "./urls-list";
 
 export function Landing() {
   return (
-    <main className="container relative border-x py-12">
-      <div className="mb-20 space-y-6 text-left">
-        <h1 className="font-black text-5xl leading-[1.1] tracking-tighter md:text-6xl lg:text-7xl">
-          Long URLs?
-          <br />
-          Snap 'em short.
-        </h1>
+    <main className="container relative p-0">
+      <div className="container relative border-x py-12">
+        <div className="absolute inset-0 -z-10 opacity-30">
+          <Background waveColor={[1, 1, 1]} />
+        </div>
+        <div className="mb-20 space-y-6 text-left">
+          <h1 className="font-black text-5xl leading-[1.1] tracking-tighter md:text-6xl lg:text-7xl">
+            Long URLs?
+            <br />
+            Snap 'em short.
+          </h1>
 
-        <p className="mx-auto max-w-2xl text-lg text-muted-foreground leading-relaxed">
-          Transform unwieldy URLs into clean, shareable links in seconds. No
-          fuss, no tracking, just pure speed.
-        </p>
-      </div>
+          <p className="mx-auto max-w-2xl text-lg text-muted-foreground leading-relaxed">
+            Transform unwieldy URLs into clean, shareable links in seconds. No
+            fuss, no tracking, just pure speed.
+          </p>
+        </div>
 
-      <div className="space-y-5">
-        <Shortener />
-        <UrlsList />
+        <div className="space-y-5">
+          <Shortener />
+          <UrlsList />
+        </div>
       </div>
     </main>
   );
